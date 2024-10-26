aa4ca2c34efb2d85f60d974d953089ca6d135ae4
unknown <joshua@goykhman.com>
2024-10-26 14:14:06 -0400

    Added java fx files

--- src/main/java/com/example/project3softmeth/ClinicManagerMain.java
+++ src/main/java/com/example/project3softmeth/ClinicManagerMain.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ClinicManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ClinicManagerMain.class.getResource("clinic-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Clinic Manager!");
         stage.setScene(scene);
         stage.show();
     }
